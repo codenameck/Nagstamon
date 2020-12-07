74160140d7b43d8ce12bdfa3e30471c49d716a5e
CK Kitisopakul <chupong.kitisopakul@gmail.com>
2020-12-08 10:29:01 +1100

    attempt, acknowledge to SensuGo req, acknowledge and disabled-check filtering, https req verify

--- Nagstamon/Servers/SensuGo.py
+++ Nagstamon/Servers/SensuGo.py
@@ -7,7 +7,7 @@ from Nagstamon.thirdparty.sensugo_api import SensuGoAPI, SensuGoAPIException
 
 # ckk for debugging
 import debugpy
-from datetime import datetime 
+from datetime import datetime
 
 debugpy.listen(5678)
 print("Waiting for debugger attach")
@@ -15,6 +15,8 @@ debugpy.wait_for_client()
 
 class SensuGoServer(GenericServer):
     TYPE = 'SensuGo'
+    MENU_ACTIONS = ['Acknowledge']
+
     _authentication = 'basic'
     _api_url = ''
     _sensugo_api = None
@@ -22,52 +24,61 @@ class SensuGoServer(GenericServer):
     def __init__(self, **kwds):
         GenericServer.__init__(self, **kwds)
         self._api_url = conf.servers[self.get_name()].monitor_cgi_url
-        self._sensugo_api = SensuGoAPI(self._api_url)
+        self.reset_HTTP()
 
     def init_HTTP(self):
         GenericServer.init_HTTP(self)
         self._setup_sensugo_api()
 
+    def reset_HTTP(self):
+        self._sensugo_api = SensuGoAPI(self._api_url)
+
     def _setup_sensugo_api(self):
         if not self._sensugo_api.has_acquired_token():
-            self._sensugo_api.auth(self.username, self.password)
+            if self.custom_cert_use:
+                verify = self.custom_cert_ca_file
+            else:
+                verify = not self.ignore_cert
+
+            try:
+                self._sensugo_api.auth(self.username, self.password, verify)
+            except Exception:
+                print('ckk error is here')
+                self.Error(sys.exc_info())
 
     def _get_status(self):
-        response_code, events = self._sensugo_api.get_all_events()
-        print(response_code)
-        print('ckk _get_status at: ' + str(datetime.now()))
-
+        print('_get_status at: ' + str(datetime.now()))
         try:
+            response_code, events = self._sensugo_api.get_all_events()
             self._create_services(events)
         except Exception:
             result, error = self.Error(sys.exc_info())
-            print(f'SensuGo response code: {response_code}')
             print(traceback.format_exc())
             return Result(result=result, error=error)
         return Result()
-    
+
     def _create_services(self, events):
         for event in events:
-            service = self._parse_event_to_service(event)        
+            service = self._parse_event_to_service(event)
             self._insert_service_to_hosts(service)
 
-    def _parse_event_to_service(self, event): 
+    def _parse_event_to_service(self, event):
         # debugpy.breakpoint()
         service = GenericService()
         service.host = event['entity']['metadata']['namespace']
         service.name = event['check']['metadata']['name']
         service.status = SensuGoAPI.parse_check_status(event['check']['status'])
-        service.last_check = datetime.fromtimestamp(int(event['timestamp'])).strftime('%Y-%m-%d %H:%M:%S') 
+        service.last_check = datetime.fromtimestamp(int(event['timestamp'])).strftime('%Y-%m-%d %H:%M:%S')
         service.duration = "05m 15s"
-        service.status_information = event['check']['output'] 
-        service.attempt = 'xxx'
-        service.passiveonly = False
-        service.notifications_disabled = False
+        service.status_information = event['check']['output']
+        service.acknowledged = event['check']['is_silenced']
+        service.notifications_disabled = event['check']['is_silenced']
+        service.attempt = str(event['check']['occurrences']) + '/1'
+        service.passiveonly = event['check']['publish']
         service.flapping = False
-        service.acknowledged = False
         service.scheduled_downtime = False
         return service
-    
+
     def _insert_service_to_hosts(self, service: GenericService):
         service_host = service.get_host_name()
         if service_host not in self.new_hosts:
@@ -76,3 +87,18 @@ class SensuGoServer(GenericServer):
             self.new_hosts[service_host].site = service.site
 
         self.new_hosts[service_host].services[service.name] = service
+
+    def set_acknowledge(self, info_dict):
+        silenece_args = {
+            'metadata': {
+                'name': info_dict['service'],
+                'namespace': info_dict['host']
+            },
+            'expire': -1,
+            'expire_on_resolve': True,
+            'creator': info_dict['author'],
+            'reason': info_dict['comment'],
+            'check': info_dict['service']
+        }
+        self._sensugo_api.create_or_update_silence(silenece_args)
+
--- Nagstamon/thirdparty/sensugo_api.py
+++ Nagstamon/thirdparty/sensugo_api.py
@@ -11,8 +11,8 @@ class SensuGoAPIException(Exception):
 class SensuGoAPI(object):
     _base_api_url = ''
     _refresh_token = ''
-    _session = None 
- 
+    _session = None
+
     GOOD_RESPONSE_CODES = (200, 201, 202, 204)
 
     SEVERITY_CHECK_STATUS = {
@@ -25,20 +25,20 @@ class SensuGoAPI(object):
     def __init__(self, base_api_url):
         self._base_api_url = base_api_url
         self._session = requests.Session()
-        
-    def auth(self, username, password):
+
+    def auth(self, username, password, verify):
         response = self._session.get(
-            f'{self._base_api_url}/auth', 
-            verify=False, 
+            f'{self._base_api_url}/auth',
+            verify=verify,
             auth=HTTPBasicAuth(username, password))
-        
+
         self._update_local_tokens(response)
 
     def _refresh_access_token(self):
         response = self._session.post(
             f'{self._base_api_url}/auth/token',
             json={'refresh_token': self._refresh_token})
-        
+
         self._update_local_tokens(response)
 
     def _update_local_tokens(self, response):
@@ -51,26 +51,30 @@ class SensuGoAPI(object):
         else:
             logger.error(f'Response code: {response.status_code} {response.text}')
             raise SensuGoAPIException('API returned bad request')
-    
+
     def get_all_events(self):
         self._refresh_access_token()
         response = self._session.get(f'{self._base_api_url}/api/core/v2/events')
         return (response.status_code, response.json())
-    
+
     def has_acquired_token(self):
         return self._refresh_token != ''
-    
+
     @staticmethod
     def parse_check_status(status_code):
         status = SensuGoAPI.SEVERITY_CHECK_STATUS[3]
         if status_code in SensuGoAPI.SEVERITY_CHECK_STATUS:
             status = SensuGoAPI.SEVERITY_CHECK_STATUS[status_code]
+        return status
 
-        return status 
+    def create_or_update_silence(self, kwargs):
+        namespace = kwargs['metadata']['namespace']
+        check = kwargs['metadata']['name']
+        silence_api = f'/api/core/v2/namespaces/{namespace}/silenced/{check}'
+        self._session.put(self._base_api_url + silence_api, json=kwargs)
 
 if __name__ == '__main__':
-    from datetime import datetime 
-
+    from datetime import datetime
     print('--- In sensugo_api main ---')
-    
     print('sensugo_api now: ' + str(datetime.now()))
+
